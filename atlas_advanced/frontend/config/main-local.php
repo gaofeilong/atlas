7f0c6aeb9c1f028a66daf05c2402f3252e375d40
gfl <gfl198810@163.com>
2017-09-23 19:43:23 +0800

    all

--- atlas_advanced/frontend/config/main-local.php
+++ atlas_advanced/frontend/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'nUtD7N3rt2F0sM6efOVkXEiO5Xhofkab',
+            'cookieValidationKey' => 'S0s1CLsCCa533N1ZAXt41wVrlqlwI3wr',
         ],
     ],
 ];
