590d50dcebb082a6f0ec01ca3e374dc20426ce1a
gfl <gfl198810@163.com>
2017-09-23 18:51:14 +0800

    del .gitignore

---
+++ atlas_advanced/common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=atlas',
+            'username' => 'gfl',
+            'password' => 'gfl',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
---
+++ atlas_advanced/frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'Vh7d2RPwiLdSJobu8q0cbPEDd1-78SSf',
+        ],
+    ],
+];
