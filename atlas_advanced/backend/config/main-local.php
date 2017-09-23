dd4eaad126b832fb240a74d2de00d6fcdd979699
gfl <gfl198810@163.com>
2017-09-23 18:58:23 +0800

    all

--- atlas_advanced/backend/config/main-local.php
+++ atlas_advanced/backend/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'A1RJPjv8EwGPzLSONzsjPC3k_WNq8uJM',
+            'cookieValidationKey' => 'tDI8egXPxKv3suCwn3XA04noGkvna5oa',
         ],
     ],
 ];
--- atlas_advanced/frontend/config/main-local.php
+++ atlas_advanced/frontend/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Vh7d2RPwiLdSJobu8q0cbPEDd1-78SSf',
+            'cookieValidationKey' => 'cN5VTsKJpIi2vi11ShC8ml_r2uIdWYbZ',
         ],
     ],
 ];
