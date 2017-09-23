5e22a4bdfdcc88058e1814cd207d57682d596d29
gfl <gfl198810@163.com>
2017-09-23 19:25:58 +0800

    add all

---
+++ atlas_advanced/backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'br0OHOU_bI1fBkKHGyKfLE1nyiT74__f',
+        ],
+    ],
+];
