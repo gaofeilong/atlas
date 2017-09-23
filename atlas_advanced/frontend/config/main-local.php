ce51a365f93fc3527b7acf92b3239cf29eb19485
gaofeilong <gaofeilong@360.cn>
2017-09-23 19:17:35 +0800

    del atlas_advanced

--- atlas_advanced/frontend/config/main-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'cN5VTsKJpIi2vi11ShC8ml_r2uIdWYbZ',
-        ],
-    ],
-];
